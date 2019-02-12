98713cba11c7ff14ce80e8ea8e194b0565904731
Mikhail <mikerar329@gmail.com>
2019-02-12 19:33:16 +0300

    initial commit

---
+++ src/Main1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Main  {
+    public static void main(String[] args) {
+        nums();
+    }
+        public static int nums() {
+        try {
+            Scanner scan = new Scanner(System.in);
+            int a = scan.nextInt(), b = scan.nextInt(), c = scan.nextInt();
+            if (a == b && b == c && a == c) {
+                System.out.println("Все числа равны");
+            } else if (a != b && a != c && b != c) {
+                System.out.println("Все числа различны");
+            } else System.out.println("Не все числа равны");
+        }
+        catch (Exception ex){
+            System.out.println("Error");
+            return  -1;
+        }
+        return 0;
+       }
+
+}
