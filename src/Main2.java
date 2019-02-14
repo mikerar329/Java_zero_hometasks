3c3ff6462d00f9403a46b6dd76959bd175ce315c
Mikhail <mikerar329@gmail.com>
2019-02-14 17:16:07 +0300

    add input checks[3]

--- src/Main2.java
+++ src/Main2.java
@@ -4,6 +4,15 @@ class Main2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         double kg = scan.nextDouble();
+        boolean right = true;
+
+        while (right == true) {
+            if (kg < 0 ) {
+                System.out.println("Mass might be positive");
+                kg = scan.nextInt();
+            }
+            else right = false;
+        }
         System.out.println("mg = " + kg*1000000);
         System.out.println("tons = " + kg/1000);
         System.out.println("g = " + kg*1000);
